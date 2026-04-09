98cc983f3880cb51fa133e833a5ec432b9841f5e
Aashu-Dalal <aashudalal2005@gmail.com>
2026-04-08 20:56:06 -0700

    Added CSS grid for perfect positiong of data

--- dynamicWebsite/script.js
+++ dynamicWebsite/script.js
@@ -1,3 +1,19 @@
+let title = document.querySelector("title");
+let views = document.querySelector("views");
+let duration = document.querySelector("duration");
+let cName = document.querySelector("cName");
+let timeAgo = document.querySelector("timeAgo");
 function thumbnail(title , views, duration, cName, timeAgo){
-    
+    title.innerText = title;
+    views.innerText = views;
+    duration.innerText = duration;
+    cName.innerText = cName;
+    timeAgo.innerText = timeAgo;
 }
+setData(
+    "CSS Exercise 5 - Design Layout",
+    "283K views",
+    "7:16",
+    "Sigma Web Dev",
+    "2 years ago"
+);
